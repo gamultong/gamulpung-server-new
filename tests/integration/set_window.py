1d9922254ac34d2651912cd194066811d87e7ff7
byudojin <byundojin0216@gmail.com>
2025-11-05 12:06:53 +0000

    teardown에서 handler 정리

--- tests/integration/set_window.py
+++ tests/integration/set_window.py
@@ -6,7 +6,7 @@ from data.payload import ServerMessage
 from data.cursor import Cursor
 from data.conn import Message
 from unittest import TestCase
-from unittest.mock import MagicMock
+from .map.case1 import case_1_map, CLOSED_TILE, OPENED_TILE
 from server import app
 from typing import cast
 from unittest.mock import AsyncMock, call, patch
@@ -22,69 +22,6 @@ clinetmanager = (
     TestClientManager(app)
     .append_client(CL_A)
 )
-
-
-OPENED_TILE = Tile(
-    is_open=True,
-    is_mine=False,
-    is_flag=False,
-    number=0
-).data
-
-CLOSED_TILE = Tile(
-    is_open=False,
-    is_mine=False,
-    is_flag=False,
-    number=0
-).data
-
-data1 = bytearray([
-    CLOSED_TILE, CLOSED_TILE, CLOSED_TILE,
-    CLOSED_TILE, OPENED_TILE, OPENED_TILE,
-    CLOSED_TILE, OPENED_TILE, OPENED_TILE,
-])
-data2 = bytearray([
-    CLOSED_TILE, CLOSED_TILE, CLOSED_TILE,
-    OPENED_TILE, OPENED_TILE, CLOSED_TILE,
-    OPENED_TILE, OPENED_TILE, CLOSED_TILE,
-])
-data3 = bytearray([
-    CLOSED_TILE, OPENED_TILE, OPENED_TILE,
-    CLOSED_TILE, OPENED_TILE, OPENED_TILE,
-    CLOSED_TILE, CLOSED_TILE, CLOSED_TILE,
-])
-data4 = bytearray([
-    OPENED_TILE, OPENED_TILE, CLOSED_TILE,
-    OPENED_TILE, OPENED_TILE, CLOSED_TILE,
-    CLOSED_TILE, CLOSED_TILE, CLOSED_TILE,
-])
-
-tiles1 = Tiles(data1, 3, 3)
-tiles2 = Tiles(data2, 3, 3)
-tiles3 = Tiles(data3, 3, 3)
-tiles4 = Tiles(data4, 3, 3)
-
-point1 = Point(-1, 0)
-point2 = Point(0, 0)
-point3 = Point(-1, -1)
-point4 = Point(0, -1)
-
-"""
-CCC|CCC
-COO|OOC
-COO|OOC
--------
-COO|OOC
-COO|OOC
-CCC|CCC
-"""
-BoardHandler.section_dict = {
-    point1: Section(point1, tiles1),
-    point2: Section(point2, tiles2),
-    point3: Section(point3, tiles3),
-    point4: Section(point4, tiles4)
-}
-
 
 """
 CCCCC
@@ -105,9 +42,16 @@ jungdap = Tiles(
 
 
 class SetWindowScenario(TestCase):
+    def setUp(self) -> None:
+        BoardHandler.section_dict = case_1_map()
+        Config.LENGTH = 3  # type:ignore
+
+    def tearDown(self) -> None:
+        BoardHandler.section_dict = {}
+        Config.LENGTH = 100
+
     @clinetmanager
     def test_normal(self, tcm: TestClientManager):
-        Config.LENGTH = 3
 
         cl_a = tcm.get_client(CL_A)
         cl_a.ws.send_json(EXAMPLE_MSG)
