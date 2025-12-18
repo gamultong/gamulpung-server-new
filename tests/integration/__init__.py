6faeef52ca831b21a7999fcea83ba5af924bc393
byudojin <byundojin0216@gmail.com>
2025-12-18 14:35:21 +0000

    파일 정리

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -1,15 +1,5 @@
 from receiver import *
 
-# Legacy unittest tests (moved to legacy/)
-# from .chat import ChatScenario
-# from .join import JoinScenario
-# from .quit import QuitScenario
-# from .set_window import SetWindowScenario
-# from .move import MoveScenario
-# from .open_tile import OpenTilesScenario
-# from .set_flag import SetFlagScenario
-# from .explosion import ExplosionScenario
-
 if __name__ == "__main__":
     from unittest import main
     main()
