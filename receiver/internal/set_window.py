d557b93485887703a8e978798a7060de25167b1b
byudojin <byundojin0216@gmail.com>
2026-01-02 13:18:29 +0000

    버그 수정

--- receiver/internal/set_window.py
+++ receiver/internal/set_window.py
@@ -21,6 +21,7 @@ async def set_window_receiver(event: SET_WINDOW_EVENT):
     cursor = await CursorHandler.get_by_id(id)
     window_range = cursor.get_window_range()
 
+    # 타일 정보 조회 및 전송
     tiles = await BoardHandler.fetch(window_range)
 
     elem = ServerMessage.TilesState.Elem(
@@ -28,11 +29,23 @@ async def set_window_receiver(event: SET_WINDOW_EVENT):
         range=window_range
     )
 
-    _event = Event(
+    tiles_event = Event(
         event_name=ServerEvent.TILES_STATE,
         payload=ServerMessage.TilesState(
             [elem]
         )
     )
 
-    await ConnectionHandler.multicast([id], _event)
+    await ConnectionHandler.multicast([id], tiles_event)
+
+    # 커서 정보 조회 및 전송
+    cursors = await CursorHandler.get_cursors_by_cursor_window(cursor)
+
+    cursors_event = Event(
+        event_name=ServerEvent.CURSORS_STATE,
+        payload=ServerMessage.CursorsState(
+            cursors
+        )
+    )
+
+    await ConnectionHandler.multicast([id], cursors_event)
